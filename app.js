7f9d0f13aa8f34fb1e31823be82e7c8068905532
Dmitry <melkdim@list.ru>
2023-05-16 20:20:56 +0500

    add

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //card-block
 const myAtropos = Atropos({
   el: '.card-block-js',
-  activeOffset: 10,
+  activeOffset: 40,
+  shadowScale: 1.05,
   alwaysActive: true,
+
+  rotate: true,
   rotateXMax: 50,
   rotateYMax: 50,
   activeOffset: 30,
   highlight: true,
+
+
 });
+
+
